297f8189ab9ef2671b638547067bc4f06b8d57d9
Jothikumar <35065074+Jothikumarsk@users.noreply.github.com>
2020-01-11 21:20:17 -0500

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-var db = require('./db');
-const cors = require('cors')
-var UserController = require('./user/UserController');
-app.use('*', cors());
-app.use('/users', UserController);
-
-module.exports = app;
